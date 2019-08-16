415a6f145e84a9b720087ee9b33cae26168e8b56
Alexandria Pugia <alex.pugia816@hotmail.com>
2019-08-16 04:52:11 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,10 +7,7 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-      {:railroads => {
-        :pieces => 4, :rent_in_dollars => {
-          
-        }}}
+      {:railroads => {:pieces => 4, :rent_in_dollars => {:one => 1, :two => 2, :three => 3, :four =>4}}}
 end
 
 def monopoly_with_fourth_tier
